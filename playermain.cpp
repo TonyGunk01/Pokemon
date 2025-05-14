f4c0f2b758369f34e0108c9667d92efb8ddf6d2d
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-14 11:54:06 +0530

    First changes
    Creating main function

---
+++ playermain.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Enter your name";
+	string player_name;
+
+	cin >> player_name;
+
+	cout << "Great start " << player_name << ", looks like you have understood the main() function properly now!";
+
+}
