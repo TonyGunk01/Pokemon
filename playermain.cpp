808f823cf21931d3d84a23b5f333649d14ce6ca0
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-14 12:53:14 +0530

    creating pokemon game
    first convo with prof oak

--- playermain.cpp
+++ playermain.cpp
@@ -3,11 +3,32 @@ using namespace std;
 
 void main()
 {
-	cout << "Enter your name";
-	string player_name;
+	cout << "Welcome to the world of Pokemon! I am Professor Oak.\n";
+	cout << "You can choose one of the following Pokemon:\n";
+	cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
 
-	cin >> player_name;
+	int ch;
+	cin >> ch;
 
-	cout << "Great start " << player_name << ", looks like you have understood the main() function properly now!";
+	if (ch == 1)
+	{
+		cout << "You chose Bulbasaur! A wise choice.\n";
+	}
 
+	else if (ch == 2)
+	{
+		cout << "You chose Charmander! A fiery choice.\n";
+	}
+
+	else if (ch == 3)
+	{
+		cout << "You chose Squirtle! A cool choice.\n";
+	}
+
+	else
+	{
+		cout < "Invalid choice. Please restart the game.\n";
+	}
+
+	return 0;
 }
