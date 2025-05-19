519fecff0aead72c54905fd4f927f2f27b0db485
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-19 12:52:57 +0530

    adding header.hpp

--- main.cpp
+++ main.cpp
@@ -85,7 +85,7 @@ class Player
             chosenPokemon = Pokemon();
         }
 
-        Player(std::string p_name, Pokemon p_chosenPokemon) 
+        Player(string p_name, Pokemon p_chosenPokemon) 
         {
             name = p_name;
             chosenPokemon = p_chosenPokemon;
@@ -119,96 +119,96 @@ class Player
 
 class ProfessorOak 
 {
-public:
-    string name;
+    public:
+        string name;
 
-    ProfessorOak(string p_name) 
-    { 
-        name = p_name; 
-    }
+        ProfessorOak(string p_name) 
+        { 
+            name = p_name; 
+        }
 
-    void greetPlayer(Player& player) 
-    {
-        cout << name << ": Hello there! Welcome to the world of Pokemon!\n";
-        waitForEnter();
+        void greetPlayer(Player& player) 
+        {
+            cout << name << ": Hello there! Welcome to the world of Pokemon!\n";
+            waitForEnter();
 
-        cout << name << ": My name is Oak. People call me the Pokemon Professor!\n";
-        waitForEnter();
+            cout << name << ": My name is Oak. People call me the Pokemon Professor!\n";
+            waitForEnter();
 
-        cout << name << ": But enough about me. Let's talk about you!\n";
-        waitForEnter();
-    }
+            cout << name << ": But enough about me. Let's talk about you!\n";
+            waitForEnter();
+        }
 
-    void offerPokemonChoices(Player& player) 
-    {
-        cout << name << ": First, tell me, what’s your name? \t [Please Enter Your Name]\n";
-        getline(std::cin, player.name);
+        void offerPokemonChoices(Player& player) 
+        {
+            cout << name << ": First, tell me, what’s your name? \t [Please Enter Your Name]\n";
+            getline(std::cin, player.name);
 
-        cout << name << ": Ah, " << player.name << "! What a fantastic name!\n";
-        waitForEnter();
+            cout << name << ": Ah, " << player.name << "! What a fantastic name!\n";
+            waitForEnter();
 
-        cout << name << ": You must be eager to start your adventure. But first, you’ll need a Pokemon of your own!\n";
-        waitForEnter();
+            cout << name << ": You must be eager to start your adventure. But first, you’ll need a Pokemon of your own!\n";
+            waitForEnter();
 
-        cout << name << ": I have three Pokemon here with me. They’re all quite feisty!\n";
-        waitForEnter();
+            cout << name << ": I have three Pokemon here with me. They’re all quite feisty!\n";
+            waitForEnter();
 
-        cout << name << ": Choose wisely...\n";
-        cout << "1. Charmander - The fire type. A real hothead!\n";
-        cout << "2. Bulbasaur - The grass type. Calm and collected!\n";
-        cout << "3. Squirtle - The water type. Cool as a cucumber!\n";
+            cout << name << ": Choose wisely...\n";
+            cout << "1. Charmander - The fire type. A real hothead!\n";
+            cout << "2. Bulbasaur - The grass type. Calm and collected!\n";
+            cout << "3. Squirtle - The water type. Cool as a cucumber!\n";
 
-        int choice;
-        cout << name << ": So, which one will it be? Enter the number of your choice: ";
-        cin >> choice;
+            int choice;
+            cout << name << ": So, which one will it be? Enter the number of your choice: ";
+            cin >> choice;
 
-        player.choosePokemon(choice);
-        waitForEnter();
-    }
+            player.choosePokemon(choice);
+            waitForEnter();
+        }
 
-    void explainMainQuest(Player& player) 
-    {
-        clearConsole();
+        void explainMainQuest(Player& player) 
+        {
+            clearConsole();
 
-        cout << "Professor Oak: " << player.name << "!, I am about to explain you about your upcoming grand adventure.\n";
-        waitForEnter();
+            cout << "Professor Oak: " << player.name << "!, I am about to explain you about your upcoming grand adventure.\n";
+            waitForEnter();
 
-        cout << "Professor Oak: You see, becoming a Pokémon Master is no easy feat. It takes courage, wisdom, and a bit of luck!\n";
-        waitForEnter();
+            cout << "Professor Oak: You see, becoming a Pokémon Master is no easy feat. It takes courage, wisdom, and a bit of luck!\n";
+            waitForEnter();
 
-        cout << "Professor Oak: Your mission, should you choose to accept it and trust me, you really don’t have a choice, is to collect all the Pokémon Badges and conquer the Pokémon League.\n";
-        waitForEnter();
+            cout << "Professor Oak: Your mission, should you choose to accept it and trust me, you really don’t have a choice, is to collect all the Pokémon Badges and conquer the Pokémon League.\n";
+            waitForEnter();
 
-        cout << "\n"  << player.name  << ": Wait... that sounds a lot like every other Pokémon game out there...\n";
-        waitForEnter();
+            cout << "\n"  << player.name  << ": Wait... that sounds a lot like every other Pokémon game out there...\n";
+            waitForEnter();
 
-        cout << "Professor Oak: Shhh! Don't break the fourth wall, " << player.name << "! This is serious business!\n";
-        waitForEnter();
+            cout << "Professor Oak: Shhh! Don't break the fourth wall, " << player.name << "! This is serious business!\n";
+            waitForEnter();
 
-        cout << "\nProfessor Oak: To achieve this, you’ll need to battle wild Pokémon, challenge gym leaders, and of course, keep your Pokémon healthy at the PokeCenter.\n";
-        waitForEnter();
+            cout << "\nProfessor Oak: To achieve this, you’ll need to battle wild Pokémon, challenge gym leaders, and of course, keep your Pokémon healthy at the PokeCenter.\n";
+            waitForEnter();
 
-        cout << "Professor Oak: Along the way, you'll capture new Pokémon to strengthen your team. Just remember, there’s a limit to how many Pokémon you can carry, so choose wisely!\n";
-        waitForEnter();
+            cout << "Professor Oak: Along the way, you'll capture new Pokémon to strengthen your team. Just remember, there’s a limit to how many Pokémon you can carry, so choose wisely!\n";
+            waitForEnter();
 
-        cout << "\n" << player.name << ": Sounds like a walk in the park... right?\n";
-        waitForEnter();
+            cout << "\n" << player.name << ": Sounds like a walk in the park... right?\n";
+            waitForEnter();
 
-        cout << "Professor Oak: Hah! That’s what they all say! But beware, young Trainer, the path to victory is fraught with challenges. And if you lose a battle... well, let’s just say you'll be starting from square one.\n";
-        waitForEnter();
+            cout << "Professor Oak: Hah! That’s what they all say! But beware, young Trainer, the path to victory is fraught with challenges. And if you lose a battle... well, let’s just say you'll be starting from square one.\n";
+            waitForEnter();
 
-        cout << "\nProfessor Oak: So, what do you say? Are you ready to become the next Pokémon Champion?\n";
-        waitForEnter();
+            cout << "\nProfessor Oak: So, what do you say? Are you ready to become the next Pokémon Champion?\n";
+            waitForEnter();
 
-        cout << "\n" << player.name << ": Ready as I’ll ever be, Professor!\n";
-        waitForEnter();
+            cout << "\n" << player.name << ": Ready as I’ll ever be, Professor!\n";
+            waitForEnter();
 
-        cout << "\nProfessor Oak: That’s the spirit! Now, your journey begins...\n";
-        waitForEnter();
+            cout << "\nProfessor Oak: That’s the spirit! Now, your journey begins...\n";
+            waitForEnter();
 
-        cout << "Professor Oak: But first... let's just pretend I didn't forget to set up the actual game loop... Ahem, onwards!\n";
-        waitForEnter();
-    }
+            cout << "Professor Oak: But first... let's just pretend I didn't forget to set up the actual game loop... Ahem, onwards!\n";
+            waitForEnter();
+        }
 };
 
 void gameLoop(Player& player) 
@@ -293,4 +293,5 @@ int main()
     gameLoop(player);
 
     return 0;
-}
+    
+#include "header.hpp"
