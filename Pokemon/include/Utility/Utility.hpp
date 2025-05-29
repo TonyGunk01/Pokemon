43bb4cd5444f3d9e2481aeb20062ec43c8d782cc
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-29 19:59:17 +0530

    Updating Utility.hpp

--- Pokemon/include/Utility/Utility.hpp
+++ Pokemon/include/Utility/Utility.hpp
@@ -1,1 +1,11 @@
+#pragma once
+#include <iostream>
+#include <limits>
 
+class Utility
+{
+    public:
+        static void clearConsole();
+        static void waitForEnter();
+        static void clearInputBuffer();
+};
