72e47224486d0390fcd7ed0a4f540b3c9ca60864
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-29 19:20:23 +0530

    shifting from root to Pokemon folder

---
+++ Pokemon/Player.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include "Pokemon.hpp"
+
+using namespace std;
+
+class Pokemon;
+
+class Player
+{
+	public:
+		string name;
+		Pokemon chosenPokemon;
+
+		Player();
+
+		Player(string p_name, Pokemon p_chosenPokemon);
+
+		void choosePokemon(int choice);
+};
