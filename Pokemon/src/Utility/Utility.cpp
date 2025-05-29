323c9be4bf7e3372258240fea215f0a60f1644f6
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-29 19:50:20 +0530

    Updating Utility.cpp

--- Pokemon/src/Utility/Utility.cpp
+++ Pokemon/src/Utility/Utility.cpp
@@ -1,1 +1,26 @@
+#include "../../include/Utility/Utility.hpp"
 
+#include <iostream>  
+#include <limits>  
+
+using namespace std;  
+
+void Utility::clearConsole()  
+{  
+    #ifdef _WIN32  
+        system("cls");  
+    #else  
+        system("clear");  
+    #endif  
+}  
+
+void Utility::waitForEnter()  
+{  
+    cout << "Press Enter to continue..." << endl;  
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');  
+}  
+
+void Utility::clearInputBuffer()  
+{  
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');  
+}
