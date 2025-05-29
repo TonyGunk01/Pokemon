40cbe479a870b89573367f0ccce8ee2fdfdb0e1f
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-29 19:55:21 +0530

    Updating main.cpp

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include "include/Character/ProfessorOak.hpp"
+#include "include/Main/Game.hpp"
+#include "include/Character/Player/Player.hpp"
 
-int main() {
+#include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    ProfessorOak professor("Professor Oak");
+    Player player;
+
+    professor.greetPlayer(player);
+	professor.offerPokemonChoices(player);
+
+    professor.explainMainQuest(player);
+
+    Game game;
+    game.gameLoop(player);
 
     return 0;
+
 }
