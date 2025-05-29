ba478a40d61a555eaf577e882ecc27c07bdb089f
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-29 19:51:38 +0530

    Updating Game.cpp

--- Pokemon/src/Main/Game.cpp
+++ Pokemon/src/Main/Game.cpp
@@ -1,1 +1,96 @@
+#include "../../include/Main/Game.hpp"
+#include "../../include/Character/Player/Player.hpp"
+#include "../../include/Pokemon/PokemonType.hpp"
+#include "../../include/Utility/Utility.hpp"
+#include "../../include/Battle/WildEncounterManager.hpp"
+#include "../../include/Battle/BattleManager.hpp" // Added this include
 
+#include <iostream>
+
+using namespace std;
+
+Game::Game() 
+{
+    forestGrass = 
+    { 
+        "Forest", 
+        {
+            Pokemon("Pidgey", PokemonType::NORMAL, 40, 10), 
+            Pokemon("Caterpie", PokemonType::BUG, 35, 5), 
+            Pokemon("Zubat", PokemonType::POISON, 30, 8)
+        }, 
+        70
+    };
+}
+
+void Game::gameLoop(Player& player) 
+{
+    BattleManager battleManager;
+    int choice;
+    bool keepPlaying = true;
+
+    while (keepPlaying) 
+    {
+        Utility::clearConsole();
+
+        cout << "\nWhat would you like to do next, " << player.name << "?\n";
+        cout << "1. Battle Wild Pokemon\n";
+        cout << "2. Visit PokeCenter\n";
+        cout << "3. Challenge Gyms\n";
+        cout << "4. Enter Pokemon League\n";
+        cout << "5. Quit\n";
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+
+        Utility::clearInputBuffer();
+
+        switch (choice) 
+        {
+            case 1: 
+            {
+                WildEncounterManager encounterManager;
+                Pokemon encounteredPokemon = encounterManager.getRandomPokemonFromGrass(forestGrass);
+
+                battleManager.startBattle(player, encounteredPokemon);
+                break;
+            }
+
+            case 2: 
+            {
+                cout << "\nYou head to the PokeCenter\n";
+                player.chosenPokemon->heal();
+				cout << player.chosenPokemon->name << "'s health is fully restored!\n";
+                break;
+            }
+
+            case 3: 
+            {
+                cout << "\nYou march up to the Gym, but it's closed for renovations. Seems like even Gym Leaders need a break!\n";
+                break;
+            }
+
+            case 4: 
+            {
+                cout << "\nYou boldly step towards the Pokemon League... but the gatekeeper laughs and says, 'Maybe next time, champ!'\n";
+                break;
+            }
+
+            case 5: 
+            {
+                keepPlaying = false;
+                break;
+            }
+
+            default: 
+            {
+                cout << "\nThat's not a valid choice. Try again!\n";
+                break;
+            }
+        }
+
+        Utility::waitForEnter();
+    }
+
+    cout << "Goodbye, " << player.name << "! Thanks for playing!\n";
+}
