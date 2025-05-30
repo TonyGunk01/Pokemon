d9897ff842914edc5a9d176d035d4b8586de710b
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-30 10:39:35 +0530

    Implementing namespaces

--- Pokemon/src/Character/Player/Player.cpp
+++ Pokemon/src/Character/Player/Player.cpp
@@ -8,46 +8,46 @@
 
 using namespace std;
 
-Player::Player()
+N_Player::Player::Player()
 {
     name = "Trainer";
-    chosenPokemon = new Pokemon();
+    chosenPokemon = new N_Pokemon::Pokemon();
 }
 
-Player::Player(string p_name, Pokemon* p_chosenPokemon)
+N_Player::Player::Player(string p_name, N_Pokemon::Pokemon* p_chosenPokemon)
 {
     name = p_name;
     chosenPokemon = p_chosenPokemon;
 }
 
-void Player::choosePokemon(int choice)
+void N_Player::Player::choosePokemon(int choice)
 {
     delete chosenPokemon;
 
     switch ((PokemonChoice)choice)
     {
         case PokemonChoice::CHARMANDER:
-            chosenPokemon = new Pokemon("Charmander", PokemonType::FIRE, 100, 50);
+            chosenPokemon = new N_Pokemon::Pokemon("Charmander", N_PokemonType::PokemonType::Fire, 100, 50);
             break;
 
         case PokemonChoice::BULBASAUR:
-            chosenPokemon = new Pokemon("Bulbasaur", PokemonType::GRASS, 100, 45);
+            chosenPokemon = new N_Pokemon::Pokemon("Bulbasaur", N_PokemonType::PokemonType::Grass, 100, 45);
             break;
 
         case PokemonChoice::SQUIRTLE:
-            chosenPokemon = new Pokemon("Squirtle", PokemonType::WATER, 100, 40);
+            chosenPokemon = new N_Pokemon::Pokemon("Squirtle", N_PokemonType::PokemonType::Water, 100, 40);
             break;
 
         default:
-            chosenPokemon = new Pokemon("Pikachu", PokemonType::ELECTRIC, 100, 55);
+            chosenPokemon = new N_Pokemon::Pokemon("Pikachu", N_PokemonType::PokemonType::Electric, 100, 55);
             break;
     }
 
     cout << "Player " << name << " chose " << chosenPokemon->name << "!\n";
-    Utility::waitForEnter();
+    N_Utility::Utility::waitForEnter();
 }
 
-Player::~Player()
+N_Player::Player::~Player()
 {
     delete chosenPokemon;
 }
