96b71cfefcfa036bafcd5b029bf601e8d1f4c31a
TonyGunk01 <208976102+TonyGunk01@users.noreply.github.com>
2025-05-30 10:40:57 +0530

    Implementing namespaces

--- Pokemon/src/Utility/Utility.cpp
+++ Pokemon/src/Utility/Utility.cpp
@@ -5,22 +5,26 @@
 
 using namespace std;  
 
-void Utility::clearConsole()  
-{  
-    #ifdef _WIN32  
-        system("cls");  
-    #else  
-        system("clear");  
-    #endif  
-}  
+namespace N_Utility 
+{
+    void Utility::clearConsole()
+    {
+        #ifdef _WIN32  
+                system("cls");
+        #else  
+                system("clear");
+        #endif  
+    }
 
-void Utility::waitForEnter()  
-{  
-    cout << "Press Enter to continue..." << endl;  
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');  
-}  
+    void Utility::waitForEnter()
+    {
+        cout << "Press Enter to continue..." << endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
 
-void Utility::clearInputBuffer()  
-{  
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');  
+    void Utility::clearInputBuffer()
+    {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
 }
